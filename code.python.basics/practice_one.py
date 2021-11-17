e6d44163227d8ffab8d3fb21dd57008adfc8b9a2
Muzafar <taranqi1214@gmail.com>
2021-11-16 20:47:10 -0500

    update

--- code.python.basics/practice_one.py
+++ code.python.basics/practice_one.py
@@ -1,7 +1,16 @@
-#lists
-temp = [10,11,30.5]
+# lists
+temp = [10, 11, 30.5]
 print(temp)
-#dictionary
-day_time = {'morning': 6.0, 'noon': 9.0, 'evening':'10'}
-print(day_time)
 
+tempone = [10, 10.4, 'hello', [2, 5, 7]]
+print(tempone)
+print(max(temp))
+
+list = [10, 5, 4, 10, 33, 101, 4, 10, 3, 6]
+print(list.count(10))
+
+username = 'sleepingCat'
+print(username.lower())
+# dictionary
+day_time = {'morning': 6.0, 'noon': 9.0, 'evening': '10'}
+print(day_time)
