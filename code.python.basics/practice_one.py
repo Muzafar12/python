efef3efe436de7766de02e8fea42fd96845f0ac4
Muzafar <taranqi1214@gmail.com>
2022-03-07 16:36:18 -0500

    1hr learning

--- code.python.basics/practice_one.py
+++ code.python.basics/practice_one.py
@@ -1,4 +1,8 @@
+#Basics already learned varible, numbers, string and all others
+#starting from lists
+
 # lists
+#you can put anything into lists (string , numbers, boolean)
 temp = [10, 11, 30.5]
 print(temp)
 
@@ -10,8 +14,15 @@ print(type(tempone))
 list = [10, 5, 4, 10, 33, 101, 4, 10, 3, 6]
 print(list.count(10))
 
-username = 'sleepingCat'
-print(username.lower())
+friends = ["John", "Frank", "Jonny"]
+print(friends)
+#accessing based on the index
+print(friends[0]);
+print(friends[-1]);
+print(friends[-3]);
+
 # dictionary
 day_time = {'morning': 6.0, 'noon': 9.0, 'evening': '10'}
 print(day_time)
+
+
